51fe55bf448c1d5691ef9c868fac262acb29a58a
Cristiano <cristianoemiliomatos@gmail.com>
2026-06-09 21:47:16 -0300

    Crie o inicio do painel do sistema e uma função que adicionar as informações do motorista em uma pasta que guarda as informações.

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print ("luqunhas")
+print("---------------------------------")
+print("  SISTEMA DE ROTAS DE LOGISTICA  ")
+print("---------------------------------")
+print("\n")
+print("1 - Cadastarar motoristas")
+print("2 - Listar motoristas")
+print("3 - Registro de viagens")
+print("4 - total gasto com combustível e quilômetros rodados ")
+print("5 - Sair")
+
+
+def cadastrar_motoristas():
+    print("Digite as informações do motorista")
+    nome = input("Digite o nome do motorista: ")
+    idade = int(input("Digite a idade do motorista: "))
+    cpf = int(input("Digite o cpf do motorista"))
+    cnh = int(input("Digite a numeração da CNH do motorista: "))
+    with open("lista_de_motoristas.txt", "w", encoding="utf-8") as cadastro_de_motoristas:
+        cadastro_de_motoristas.write(f"Nome do motorista: {nome} | Idade: {idade} | cpf: {cpf} | cnh: {cnh}")
+cadastrar_motoristas()
