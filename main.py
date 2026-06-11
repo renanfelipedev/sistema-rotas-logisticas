e5ed62a58e1035a75d70abdfa6004368c4135011
Lucas Andrade <lucasandradesouza109@gmail.com>
2026-06-11 11:58:56 -0300

    adicao da pasta operacoes.py

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 import motoristas
 import os
 import csv
-print("---------------------------------")
-print("  SISTEMA DE ROTAS DE LOGISTICA  ")
-print("---------------------------------")
-print("\n")
-print("1 - Cadastarar motoristas")
-print("2 - Listar motoristas")
-print("3 - Registro de viagens")
-print("4 - total gasto com combustível e quilômetros rodados ")
-print("5 - Sair")
+def menu():
+    criar_arquivos()
+
+    while True:
+        print("---------------------------------")
+        print("  SISTEMA DE ROTAS DE LOGISTICA  ")
+        print("---------------------------------")
+        print("\n")
+        print("1 - Cadastarar motoristas")
+        print("2 - Listar motoristas")
+        print("3 - Registrar viagens")
+        print("4 - Listar viagens")
+        print("4 - total gasto com combustível e quilômetros rodados ")
+        print("0 - Sair")
 
 with open("motoristas.csv", "w", newline="", encoding="utf-8") as arquivo:
     motorista = csv.writer(arquivo)
