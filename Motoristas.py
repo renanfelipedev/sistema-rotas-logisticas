2065a0ec3c3cfb3946db6693a24aef37225bfa81
Cristiano <cristianoemiliomatos@gmail.com>
2026-06-11 21:07:56 -0300

    Criei a função que manda as viagens para o arquivo csv rotas e viagens

--- Motoristas.py
+++ Motoristas.py
@@ -33,4 +33,44 @@ def listar_motoristas():
         reader = csv.DictReader(cadastro)
         for motorista in reader:
             print(f"Nome: {motorista['Nome']} | Idade: {motorista['Idade']} | CPF: {motorista['CPF']} | CNH: {motorista['CNH']}")
-        
+
+
+
+
+def registar_viagem():
+    print("Digite as informações da viagem")
+
+    origem = input("Digite a origem da viagem: ")
+    destino = input("Digite o destino da viagem: ")
+    distancia = float(input("Digite a distância da viagem (km): "))
+    consumo = float(input("Digite o consumo da viagem (L): "))
+    motorista = input("Escreva o nome do motorista: ")
+
+    arquivo = "rotas_viagens.csv"
+    arquivo_existe = os.path.isfile(arquivo)
+
+    with open(arquivo, "a", newline="", encoding="utf-8") as cadastro:
+        campos = ["origem", "destino", "distancia", "consumo", "motorista"]
+
+        writer = csv.DictWriter(
+            cadastro,
+            fieldnames=campos,
+            delimiter=";"
+        )
+
+        if not arquivo_existe:
+            writer.writeheader()
+
+        writer.writerow({
+            "origem": origem,
+            "destino": destino,
+            "distancia": f"{distancia:.1f} km",
+            "consumo": f"{consumo:.1f} L",
+            "motorista": motorista
+        })
+
+    print("Viagem cadastrada com sucesso!")
+
+
+
+registar_viagem()
